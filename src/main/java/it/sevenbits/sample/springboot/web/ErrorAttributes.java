5e6e4f757ec9721db8290a88a46c9604841fd4da
Denis Nelubin <dnelubin@gmail.com>
2018-04-23 11:27:02 +0600

    Added custom error view.

---
+++ src/main/java/it/sevenbits/sample/springboot/web/ErrorAttributes.java
@@ -0,0 +1,21 @@
+package it.sevenbits.sample.springboot.web;
+
+import org.springframework.boot.autoconfigure.web.DefaultErrorAttributes;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestAttributes;
+
+import java.util.Map;
+
+@Component
+class ErrorAttributes extends DefaultErrorAttributes {
+
+    @Override
+    public Map<String, Object> getErrorAttributes(RequestAttributes requestAttributes, boolean includeStackTrace) {
+        Map<String, Object> attributes = super.getErrorAttributes(requestAttributes, includeStackTrace);
+
+        attributes.put("success", false);
+
+        return attributes;
+    }
+
+}
